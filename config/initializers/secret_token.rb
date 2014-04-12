619cebae05385c955b92f332a88a47100164726d
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2014-04-11 19:06:21 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trellino::Application.config.secret_key_base = 'c21fab27fe916f956d7d0ad07609321533658fa9e5b91379324dba9eb974468d0d240298ff0001281ff187c5077d2e2d3ad49631e57e3b4b40c5fed2c684cec5'
